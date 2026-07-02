fad3d707763e87f0212f2f16533c887341288788
Raspbsb <unionbjh@naver.com>
2026-07-02 16:23:12 +0900

    feat: refactor database connection and update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql2");
 const port = 3000;
 
 let corsOptions = {
@@ -9,8 +10,31 @@ let corsOptions = {
 
 app.use(cors(corsOptions));
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "sqlqns",
+  // db 이름 (테이블 이름 X)
+  database: "bbs",
+});
+
+db.connect();
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/list", (req, res) => {
+  const sqlQuery =
+    "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/write", (req, res) => {
+  console.log(req);
 });
 
 app.listen(port, () => {
