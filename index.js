82996318715dc05d166b87e8bdd9c191f09ddac5
Raspbsb <unionbjh@naver.com>
2026-07-06 15:37:51 +0900

    feat: implement image upload functionality and update board entry endpoints

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ const cors = require("cors");
 const app = express();
 const mysql = require("mysql2");
 const port = 3000;
+const multer = require("multer");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //json->object
+app.use(express.json()); // json -> object
+app.use(express.urlencoded({ extended: true })); // html form -> object
+app.use("/uploads", express.static("uploads")); // uploads 주소로 접속시 uploads 폴더에 접근 권한 부여
 
 let corsOptions = {
   origin: "*",
 };
 
 app.use(cors(corsOptions));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const orinalExt = file.originalname.split(".")[1];
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1000);
+    // cb(null, uniquePrefix + "-" + file.originalname);
+    cb(null, uniquePrefix + "-" + file.fieldname + "." + orinalExt);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -45,7 +61,7 @@ app.get("/view", (req, res) => {
 
   // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
   const sqlQuery =
-    "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
+    "SELECT title, content, writer, image_path, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
   db.query(sqlQuery, [id], (err, result) => {
     if (err) throw err;
     res.send(result);
@@ -53,23 +69,53 @@ app.get("/view", (req, res) => {
 });
 
 // 글쓰기
-app.post("/write", (req, res) => {
+app.post("/write", upload.single("image"), (req, res) => {
   console.log(req.body);
-  const { title, name, content } = req.body;
+  const { title, writer, content } = req.body;
+  const imagePath = req.file ? req.file.path : null; //req.file.path는 업로드된 파일의 경로
 
-  const sqlQuery = "INSERT INTO board (title,content,writer) VALUES (?,?,?);";
-  db.query(sqlQuery, [title, content, name], (err, result) => {
+  const sqlQuery = "INSERT INTO board (title,content,writer,image_path) VALUES (?,?,?,?);";
+  db.query(sqlQuery, [title, content, writer, imagePath], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
 });
 
-app.post("/update", (req, res) => {
+app.post("/update", upload.single("image"), (req, res) => {
   console.log(req.body);
-  const { name, title, content, id } = req.body;
 
-  const sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?;";
-  db.query(sqlQuery, [name, title, content, id], (err, result) => {
+  const { writer, title, content, id, remove_image } = req.body;
+  const imagePath = req.file ? req.file.path : null; // 새 이미지 정보 할당
+  const shouldRemoveImage = remove_image === "1";
+
+  let sqlQuery;
+  let params;
+
+  // const sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?;";
+
+  // 상황별 params 정의
+  // 이미지 삭제 요청 O + 새 이미지 X -> 기존 이미지 제거, image_path 값 비우기
+  if (shouldRemoveImage && !imagePath) {
+    sqlQuery = "UPDATE board SET writer=?, title=?, content=?, image_path=NULL WHERE id=?;";
+    params = [writer, title, content, id];
+  }
+  // 이미지 삭제 요청 X + 새 이미지 O -> 기존 이미지 유지, image_path에 새 이미지 업데이트
+  else if (!shouldRemoveImage && imagePath) {
+    sqlQuery = "UPDATE board SET writer=?, title=?, content=?, image_path=? WHERE id=?;";
+    params = [writer, title, content, imagePath, id];
+  }
+  // 이미지 삭제 요청 X + 새 이미지 X -> 기존 이미지 유지, 글정보만 변경
+  else if (!shouldRemoveImage && !imagePath) {
+    sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?";
+    params = [writer, title, content, id];
+  }
+  // 이미지 삭제 요청 O + 새 이미지 O -> 기존 이미지 제거, image_path에 새 이미지 업데이트
+  else if (shouldRemoveImage && imagePath) {
+    sqlQuery = "UPDATE board SET writer=?, title=?, content=?, image_path=? WHERE id=?";
+    params = [writer, title, content, imagePath, id];
+  }
+
+  db.query(sqlQuery, params, (err, result) => {
     if (err) throw err;
     res.send(result);
   });
