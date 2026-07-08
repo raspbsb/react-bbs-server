9b631b5fff8dacee4a9a90b7ba98e2e2e2b6eac5
Raspbsb <unionbjh@naver.com>
2026-07-08 10:02:32 +0900

    feat: update static file serving for uploads and improve path handling; add new image files

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mysql = require("mysql2");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 
 app.use(express.json()); // json -> object
 app.use(express.urlencoded({ extended: true })); // html form -> object
-app.use("/uploads", express.static("uploads")); // uploads 주소로 접속시 uploads 폴더에 접근 권한 부여
+// app.use("/uploads", express.static("uploads")); // uploads 주소로 접속시 uploads 폴더에 접근 권한 부여
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // /uploads 절대경로 upload 폴더에 접근 권한 부여
 
 let corsOptions = {
   origin: "*",
@@ -20,7 +21,7 @@ app.use(cors(corsOptions));
 // 파일 저장소 연결 (멀터)
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, "uploads"));
   },
   filename: function (req, file, cb) {
     const orinalExt = file.originalname.split(".")[1];
